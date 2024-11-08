cdfe2e3a6a8d2c525f715f21a97b2ccb1fc45365
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2024-11-08 23:48:04 +0600

    assigment update and added

--- src/problem-2.ts
+++ src/problem-2.ts
@@ -6,6 +6,3 @@ console.log(
   JSON.stringify(removeDuplicates([1, 2, 2, 3, 4, 4, 5])) ===
     JSON.stringify([1, 2, 3, 4, 5])
 );
-
-const result = removeDuplicates([1, 2, 2, 3, 4, 4, 5])
-console.log(result);
